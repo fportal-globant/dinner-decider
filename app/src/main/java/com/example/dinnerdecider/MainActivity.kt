6b5d2628496f42251970f5b90e7adcc8b423c92f
Fernando <fernando.portal@globant.com>
2020-07-08 16:18:53 -0300

    Commit app

---
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburguer", "Pizza", "Mc Donalds", "Pasta")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            if (newFood.isNotEmpty()) {
+                foodList.add(newFood)
+            }
+            addFoodText.text.clear()
+        }
+    }
+}
